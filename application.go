bfaac7c8bb3b735815e4edda4ad5ba685715de98
Zach Dolan <zmd989@mail.missouri.edu>
2020-08-29 14:20:51 -0500

    Create application.go

---
+++ application.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		today := time.Now().Weekday()
+		if today == time.Weekday(3) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"message": "it is Wednesday my dudes"}`))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"message": "it is not Wednesday my dudes"}`))
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "unsupported method"}`))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
